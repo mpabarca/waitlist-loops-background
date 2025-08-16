b231f09c6ec0a4dd79424a473e865bd9826c8e0d
mpabarca <mp.abarca.alvear@gmail.com>
2025-08-16 09:56:21 +0200

    Add Background color on Landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,27 +3,38 @@ import Footer from "@/components/Footer";
 import LoopsForm from "@/components/LoopsForm";
 
 export default function LandingPage() {
+  const background = "bg-gray-100";
+  const padding = "px-5 pt-5";
+
   return (
-    <main className='min-h-screen flex flex-col items-center justify-center mx-4 md:mx-auto'>
-      <div className='flex flex-col justify-center items-center text-center gap-8 my-auto pb-14'>
-        <section className='flex flex-col gap-4 items-center'>
-          {/* Logo */}
-          <img
-            src='/vite.svg'
-            alt='Logo'
-            className='w-12 h-12 md:w-15 md:h-15'
-          />
-          <EarlyAccessBadge />
-        </section>
-        <section className='flex flex-col gap-4'>
-          <h1 className='text-4xl font-bold'>Launch with Confidence</h1>
-          <p className='max-w-md'>
-            Collect emails, build hype, and validate your idea with a beautiful
-            pre-launch page.
-          </p>
-        </section>
-        <LoopsForm />
+    <main className='min-h-screen flex flex-col'>
+      <div className={`flex-1 flex ${padding}`}>
+        <div className={`w-full flex flex-1 flex-col justify-center items-center text-center rounded-xl gap-8 pb-16 ${background}`}>
+          {/* Logo + Badge */}
+          <section className='flex flex-col gap-4 items-center'>
+            <img
+              src='/vite.svg'
+              alt='Logo'
+              className='w-12 h-12 md:w-15 md:h-15'
+            />
+            <EarlyAccessBadge />
+          </section>
+
+          {/* Title & Subtitle */}
+          <section className='flex flex-col gap-4'>
+            <h1 className='text-4xl font-bold'>Launch with Confidence</h1>
+            <p className='max-w-md'>
+              Collect emails, build hype, and validate your idea with a
+              beautiful pre-launch page.
+            </p>
+          </section>
+
+          {/* Form */}
+          <LoopsForm />
+        </div>
       </div>
+
+      {/* Sticky Footer */}
       <Footer />
     </main>
   );
